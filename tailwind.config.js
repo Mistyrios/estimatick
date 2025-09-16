ee1b1a9f746cae94b6a355911aaefd31873408f0
Mistyrios <axelsalem.dev@gmail.com>
2025-09-16 17:13:30 +0200

    ETK-1: Add initial project structure and setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+        "./src/Form/*.php",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
